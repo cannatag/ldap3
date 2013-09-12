f066bb30591a2c50242815652464cd936e56912f
Giovanni Cannata &lt;python3ldap <Giovanni Cannata &lt;python3ldap@gmail.com&gt;>
2013-09-12 10:17:20 +0000

    1 Schema browser

--- python3-ldap/ldap3/protocol/schema.py
+++ python3-ldap/ldap3/protocol/schema.py
@@ -24,30 +24,101 @@ If not, see <http://www.gnu.org/licenses/>.
 from collections import namedtuple
 from os import linesep
 import re
+from OCDef import fromDefinition
+from ldap3 import CLASS_ABSTRACT, CLASS_STRUCTURAL, CLASS_AUXILIARY
 
+def quotedStringToList(string):
+        string = string.strip()
+        if string[0] == '(' and string[:-1] == ')':
+            string = string[1:-1]
+        elements = string.split()
+        return [element.strip("'").strip() for element in elements]
 
-class ObjectClassInfo(namedtuple('ObjectClassInfo', 'oid, name, description, obsolete, superior, type, mustContains, mayContains, extensions')):
-    # def __init__(self, oid, name = None, description = None, obsolete = None, superior = None, type = None, mustContains = None, mayContains = None, extensions = None):
-    #     self.oid = oid
-    #     self.name = name
-    #     self.description = description
-    #     self.obsolete = obsolete
-    #     self.superior = superior
-    #     self.type = type
-    #     self.mustContains = mustContains
-    #     self.mayContains = mayContains
-    #     self.extensions = extensions
+def oidsStringToList(string):
+        string = string.strip()
+        if string[0] == '(' and string[:-1] == ')':
+            string = string[1:-1]
+        elements = string.split('$')
+        return [element.strip() for element in elements]
 
+def extensionToTuple(string):
+        string = string.strip()
+        name, _, values = string.partition(' ')
+        return (name, quotedStringToList(values))
+
+class ObjectClassInfo():
+    def __init__(self, oid = None, name = None, description = None, obsolete = False, superior = None, kind = None, mustContain = None, mayContain = None, extensions = None, experimental = None):
+        self.oid = oid
+        self.name = name
+        self.description = description
+        self.obsolete = obsolete
+        self.superior = superior
+        self.kind = kind
+        self.mustContain = mustContain
+        self.mayContain = mayContain
+        self.extensions = extensions
+        self.experimental = experimental
+
+    def __str__(self):
+        r = self.oid
+        r += (': ' + self.name) if self.name else ''
+        r += (' [OBSOLETE]' + linesep) if self.obsolete else linesep
+        r += ('  Description: ' + self.description + linesep) if self.description else ''
+        r += ('  Type: ' + self.kind + linesep) if self.kind else ''
+        r += ('  Must contain attributes: ' + self.mustContain + linesep) if self.mustContain else ''
+        r += ('  May contain attributes: ' + self.mayContain + linesep) if self.mayContain else ''
+        r += ('  Extensions: ' + self.extensions + linesep) if self.extensions else ''
+        r += ('  Experimental: ' + self.experimental + linesep) if self.experimental else ''
+
+        return r
     @staticmethod
     def fromDefinition(objectClassDefinition):
+        if not objectClassDefinition:
+            return None
+
         if [objectClassDefinition[0] == ')' and objectClassDefinition[:-1] == ')']:
-            splitted = re.split('( NAME | DESC | OBSOLETE| SUP | ABSTRACT| STRUCTURAL| AUXILIARY| MUST | MAY | X-)', objectClassDefinition[1:-1])
+            splitted = re.split('( NAME | DESC | OBSOLETE| SUP | ABSTRACT| STRUCTURAL| AUXILIARY| MUST | MAY | X-| E-)', objectClassDefinition[1:-1])
             values = splitted[::2]
             separators = splitted[1::2]
             separators.insert(0, 'OID')
-
-            #return d
-        # return ObjectClassInfo(values[0], None, None, None, None, None, None, None, None)
+            defs = list(zip(separators, values))
+            objectClassDef = ObjectClassInfo()
+            for d in defs:
+                key = d[0].strip()
+                value = d[1].strip()
+                if key == 'OID':
+                    objectClassDef.oid = value
+                elif key == 'NAME':
+                    objectClassDef.name = quotedStringToList(value)
+                elif key == 'DESC':
+                    objectClassDef.description = value
+                elif key == 'OBSOLETE':
+                    objectClassDef.obsolete = True
+                elif key == 'SUP':
+                    objectClassDef.description = oidsStringToList(value)
+                elif key == 'ABSTRACT':
+                    objectClassDef.kind = CLASS_ABSTRACT
+                elif key == 'STRUCTURAL':
+                    objectClassDef.kind = CLASS_STRUCTURAL
+                elif key == 'AUXILIARY':
+                    objectClassDef.kind = CLASS_AUXILIARY
+                elif key == 'MUST':
+                    objectClassDef.mustContain = oidsStringToList(value)
+                elif key == 'MAY':
+                    objectClassDef.mayContain = oidsStringToList(value)
+                elif key == 'X-':
+                    if not objectClassDef.extensions:
+                        objectClassDef.extensions = list()
+                    objectClassDef.extensions.append(extensionToTuple('X-' + value))
+                elif key == 'E-':
+                    if not objectClassDef.experimental:
+                        objectClassDef.experimental = list()
+                    objectClassDef.experimental.append(extensionToTuple('E-' + value))
+                else:
+                    raise Exception('malformed Object Class Definition key:' + key)
+            return objectClassDef
+        else:
+            raise Exception('malformed Object Class Definition')
 
 
 class SchemaInfo():
@@ -62,7 +133,7 @@ class SchemaInfo():
         self.modifyTimeStamp = attributes.pop('modifyTimestamp', None)
         self.attributeTypes = attributes.pop('attributeTypes', None)
         self.ldapSyntaxes = attributes.pop('ldapSyntaxes', None)
-        self.objectClasses = attributes.pop('objectClasses', None)
+        self.objectClasses = [objectClassDef for objectClassDef in ObjectClassInfo.fromDefinition(attributes.pop('objectClasses', []))]
         self.other = attributes
 
     def __str__(self):

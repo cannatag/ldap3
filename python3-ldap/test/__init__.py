8391275b63df80ccb0fc6e1024eed5187502382f
Giovanni Cannata <cannatag@gmail.com>
2013-09-17 12:06:19 +0000

    Move to beta

--- python3-ldap/test/__init__.py
+++ python3-ldap/test/__init__.py
@@ -23,9 +23,9 @@ If not, see <http://www.gnu.org/licenses/>.
 # noinspection PyUnresolvedReferences
 from ldap3 import AUTH_SIMPLE, STRATEGY_SYNC, STRATEGY_ASYNC_THREADED
 
-test_server = 'sl10.intra.camera.it'
-test_user = 'cn=admin,o=services'
-test_password = 'camera'
+test_server = 'ldapserver'
+test_user = 'ldapuser'
+test_password = 'password'
 
 test_port = 389
 test_port_ssl = 636

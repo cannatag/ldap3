1370ce7d8ad4a7e6d457ae1b0fe0186a00ab2276
Giovanni Cannata <cannatag@gmail.com>
2013-11-13 18:54:25 +0000

    test customization

--- python3-ldap/test/__init__.py
+++ python3-ldap/test/__init__.py
@@ -24,19 +24,19 @@ If not, see <http://www.gnu.org/licenses/>.
 from Tools.Scripts.find_recursionlimit import test_getattr
 from ldap3 import AUTH_SIMPLE, STRATEGY_SYNC, STRATEGY_ASYNC_THREADED
 
-test_server = 'sl10.intra.camera.it'
-test_user = 'cn=admin,o=services'
-test_password = 'camera'
+test_server = 'server'  # the ldap server where tests executed
+test_user = 'user'  # the user that performs the tests
+test_password = 'password'  # user's password
 
-test_base = 'ou=test2,o=test'
-test_moved = 'ou=test3,o=test'
-test_name_attr = 'cn'
+test_base = 'o=test'  # base context where test objects are created
+test_moved = 'ou=moved,o=test'  # base context where  objects are moved in ModifyDN operations
+test_name_attr = 'cn'  # naming attribute for test objects
 
-test_port = 389
-test_port_ssl = 636
-test_authentication = AUTH_SIMPLE
-test_strategy = STRATEGY_SYNC
-#test_strategy = STRATEGY_ASYNC_THREADED
+test_port = 389  # ldap port
+test_port_ssl = 636  # ldap secure port
+test_authentication = AUTH_SIMPLE  # authentication type
+test_strategy = STRATEGY_SYNC  # strategy for executing tests
+#test_strategy = STRATEGY_ASYNC_THREADED  # uncomment this line to the the async strategy
 
 
 def testDnBuilder(base, name):

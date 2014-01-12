fd9f0ff01835f92449371fe46dd1f2c3320a9ad3
Giovanni Cannata &lt;python3ldap <Giovanni Cannata &lt;python3ldap@gmail.com&gt;>
2014-01-12 06:51:17 +0000

    abstraction

---
+++ python3-ldap/ldap3/abstraction/defs.py
@@ -0,0 +1,143 @@
+"""
+Created on 2014.01.11
+
+@author: Giovanni Cannata
+
+Copyright 2014 Giovanni Cannata
+
+This file is part of python3-ldap.
+
+python3-ldap is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+python3-ldap is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with python3-ldap in the COPYING and COPYING.LESSER files.
+If not, see <http://www.gnu.org/licenses/>.
+"""
+
+
+class AttrDef(object):
+    def __init__(self, name, key = None, multiValue = False, validate = None, preQuery = None, postQuery = None, default = None):
+        self.name = name
+        self.key = key or name  # key set to name if not present
+        self.multiValue = multiValue if multiValue else False
+        self.validate = validate
+        self.preQuery = preQuery
+        self.postQuery = postQuery
+        self.default = default
+
+    def __repr__(self):
+        r = 'AttrDef(name={0.name!r}'.format(self)
+        r += '' if self.key is None or self.name == self.key else ', key={0.key!r}'.format(self)
+        r += '' if self.multiValue is False else ', multiValue={0.multiValue!r}'.format(self)
+        r += '' if self.validate is None else ', validate={0.validate!r}'.format(self)
+        r += '' if self.preQuery is None else ', preQuery={0.preQuery!r}'.format(self)
+        r += '' if self.postQuery is None else ', postQuery={0.postQuery!r}'.format(self)
+        r += '' if self.default is None else ', default={0.default!r}'.format(self)
+        r += '' if self.always is False else ', always={0.always!r}'.format(self)
+        r += ')'
+
+        return r
+
+    def __str__(self):
+        return self.__repr__()
+
+    def __eq__(self, other):
+        if isinstance(other, AttrDef):
+            if self.key == other.key:
+                return True
+
+        return False
+
+    def __hash__(self):
+        if self.key:
+            return hash(self.key)
+        else:
+            return id(self)  # unique for each istance
+
+    def __setattr__(self, key, value):
+        print ('setattr', key, value)
+        if hasattr(self, 'key') and key == 'key':  # key cannot be changed because is used for __hash__
+            raise Exception('key already set')
+        else:
+            super(AttrDef, self).__setattr__(key, value)
+
+
+class ObjectDef(object):
+    """
+    Ogni attributo e' definito da un dizionario che ha come chiave il
+    nome con cui si accede all'attributo e come campi:
+    attr -> il nome dell'attributo nell'ldap
+    multivalue -> un booleano che indica se l'attributo puo' avere piu' valori
+    valida -> una funzione che valida il valore di ricerca prima di aggiungere l'attributo alla query di ricerca
+    preQuery -> una funzione da eseguire sul valore di ricerca il cui risultato viene aggiunto alla query di ricerca
+    postQuery -> una funzione da eseguire dopo la ricerca il cui risultato viene ritornato come valore del campo
+    ritornaSempre -> un booleano che indica se il valore deve essere ritornato sempre oppure solo se richiesto esplicitamente
+    default -> un valore di default da ritornare quando l'attributo non viene trovato
+    Se ritornaSempre e' impostato a False viene ritornato il valore di default
+    "2"
+    """
+
+    def __init__(self):
+        self.clear()
+
+    def add(self, attributeDef = None):
+        if hasattr(attributeDef, '__iter__'):
+            for element in attributeDef:
+                self.add(element)
+            return
+
+        if not isinstance(attributeDef, AttrDef):
+            raise Exception('invalid attribute definition')
+
+        key = attributeDef.key or attributeDef.name  # key set to name if not present
+        if key in self.attributes:
+            raise Exception('attribute already defined')
+
+        self.attributes[key] = attributeDef
+
+    def remove(self, key):
+        if key in self.attributes:
+            del self.attributes[key]
+
+    def clear(self):
+        self.attributes = dict()
+
+    def __len__(self):
+        return len(self.attributes)
+
+    def __contains__(self, item):
+        return True if item in self.attributes else False
+
+    def __repr__(self):
+        s = ''
+        for attr in self.attributes:
+            s += attr.__repr__() + ', '
+
+        return s[:-2]
+
+    def __str__(self):
+        s = ''
+        for attr in self.attributes:
+            s += attr.__str__()
+
+        return s
+
+    def __getitem__(self, item):
+        return self.attributes[item]
+
+    def __add__(self, other):
+        self.add(other)
+
+    def __sub__(self, other):
+        if isinstance(other, AttrDef):
+            self.remove(other.key)
+        elif isinstance(other, str):
+            self.remove(other)
--- python3-ldap/ldap3/abstraction/reader.py
+++ python3-ldap/ldap3/abstraction/reader.py
@@ -24,9 +24,97 @@ If not, see <http://www.gnu.org/licenses/>.
 
 
 class Reader(object):
-    def __init__(self, connection, attrDefs, query, attributes):
+    def __init__(self, connection, objectDef, query):
         self.connection = connection
         self.query = query
-        self.definition = attrDefs
-        self.attributes = attributes
+        self.definition = objectDef
 
+    def _creaDict(self, text):
+        '''
+        crea un dizionario con le coppie chiave:valore di una query
+        Il testo della query deve essere composto da coppie chiave:valore separate dalla virgola.
+        '''
+        queryDict = dict()
+        for argValueStr in text.split(','):
+            if ':' in argValueStr:
+                argValueList = argValueStr.split(':')
+                queryDict[argValueList[0].strip()] = argValueList[1].strip()
+
+        return queryDict
+
+    def _validateQueryText(self, text):
+        """
+        Elabora il testo di una query e verifica che i campi richiesti siano presenti nel dizionario
+        del reader relativo. Se non sono presenti il campo viene ignorato.
+        Se l'attributo nel dizionario ha un campo 'valida' la relativa funzione viene eseguita per
+        verificare se il valore da ricercare sia tra quelli possibili. In caso di
+        valore non validato viene generata un'eccezione.
+        Il testo della query deve essere composto da coppie chiave:valore separate dalla virgola.
+        Sono riconosciute alcune chiavi speciali che modificano il funzionamento della query:
+        'query_and_or' (con valore 'AND' oppure 'OR') -> indica che i campi di ricerca sono in AND oppure in OR
+        'query_filtro' -> esegue un generico filtro LDAP
+        'query_base' -> base su cui eseguire 'query_filtro', non puo' essere usato senza 'query_filtro'
+        'query_livello' (con valore 'SUB' oppure 'LIVELLO') -> indica il livello di esecuzione della query
+        Per eseguire un AND o un OR nello stesso attributo far precedere la chiave dal segno '&' oppure '|'
+        e dividere i valori multipli con un ';'
+        In 'query_filtro' e in 'query_base' eventuali ',' devono essere indicate con ';'
+        Un valore preceduto da un '!' indica un NOT
+        Il valore e' composto da un segno di ricerca (=, >, <, ~) e dalla chiave di ricerca
+        Ritorna il testo della query validato che puo' essere trasformato in dizionario per essere utilizzato
+        come chiave di ricerca delle funzioni _cerca dei reader di classe ldap.
+        """
+
+        queryDict = self._creaDict(text)
+        query = ''
+        for d in sorted(queryDict):
+            attr = d[1:] if d[0] in '&|' else d
+
+            if attr.lower() not in ['query_and_or', 'query_filter', 'query_base', 'query_scope']:
+                for attrDef in self.definition:
+                    if attr.lower() == attrDef.lower():
+                        attr = attrDef
+                        break
+
+                if attr in self.definition:
+                    vals = queryDict[d].split(';')
+
+                    query += d[0] + attr if d[0] in '&|' else attr
+                    query += ': '
+                    for val in vals:
+                        val = val.strip()
+                        valNot = True if val[0] == '!' else False
+                        if valNot:
+                            valSearchOperator = val[1:].lstrip()[0]
+                            value = val[1:].lstrip()[1:]
+                        else:
+                            valSearchOperator = val[0]
+                            value = val[1:].lstrip()
+
+                        if valSearchOperator not in '=<>~':
+                            raise Exception('invalid operator, must be one of =, <, > or ~')
+
+                        if self.definition[attr].validate:
+                            if not self.definition[attr].validate(value):
+                                raise Exception('validation failed for attribute %s with value %s' % (val, queryDict[d]))
+
+                        if valNot:
+                            query += '!' + valSearchOperator + value
+                        else:
+                            query += valSearchOperator + value
+
+                        query += ';'
+                    query = query[:-1]
+                    query += ', '
+            else:
+                if attr.lower() == 'query_and_or':
+                    if queryDict[d].lower() in ['and', 'or']:
+                        query += 'query_and_or: ' + queryDict[d].lower() + ', '
+                elif attr.lower() == 'query_filter':
+                    query += 'query_filter: ' + queryDict[d] + ', '
+                elif attr.lower() == 'query_base':
+                    query += 'query_base: ' + queryDict[d] + ', '
+                elif attr.lower() == 'query_scope':
+                    if queryDict[d].lower() in ['sub', 'level']:
+                        query += 'query_scope: ' + queryDict[d].lower() + ', '
+
+        return query[:-2]

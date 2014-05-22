b4b5748873e8731777a2bf1d6458e0417a474f7f
Giovanni Cannata <cannatag@gmail.com>
2014-05-22 19:30:49 +0000

    fixerd auto_bind exception

--- python3-ldap/ldap3/strategy/ldifProducer.py
+++ python3-ldap/ldap3/strategy/ldifProducer.py
@@ -85,7 +85,7 @@ class LdifProducerStrategy(BaseStrategy):
         return False
 
     def post_send_search(self, message_id):
-        raise LDAPLDIFError('this strategy cannot produce ldif-content for Search Operations')
+        raise LDAPLDIFError('LDIF-CONTENT cannot be produced for Search Operations')
 
     def _get_response(self, message_id):
         pass

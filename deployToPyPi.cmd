780ead57a42480e8d1e5d2b3a8c8028628f0b185
Giovanni Cannata <cannatag@gmail.com>
2014-11-12 00:17:21 +0100

    0.9.6.1

--- deployToPyPi.cmd
+++ deployToPyPi.cmd
@@ -1,7 +1,9 @@
-rd dist build python3-ldap\python3_ldap.egg-info /S
+rd dist build python3-ldap\python3_ldap.egg-info /S /Q
 \Python\Python34\python.exe setup.py clean
 \Python\Python34\python.exe setup.py build sdist --format=gztar upload
 \Python\Python34\python.exe setup.py build bdist_wininst upload
+\Python\Python27\python.exe setup.py build bdist_wininst upload
+\Python\Python26\python.exe setup.py build bdist_wininst upload
 \Python\Python34\python.exe setup.py build bdist_wheel upload
 \Python\Python34\python.exe setup.py build bdist_egg upload
 \Python\Python27\python.exe setup.py build bdist_egg upload

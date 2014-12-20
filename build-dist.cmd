e6dfb229a79af5afbf005a498559631f71d4eae4
Giovanni Cannata <cannatag@gmail.com>
2014-12-20 08:23:11 +0100

    updated setup

---
+++ build-dist.cmd
@@ -0,0 +1,8 @@
+rd dist build python3-ldap\python3_ldap.egg-info /S /Q
+\Python\Python34\python.exe setup.py clean
+\Python\Python34\python.exe setup.py build sdist --format=gztar
+\Python\Python34\python.exe setup.py build bdist_wininst
+\Python\Python34\python.exe setup.py build bdist_wheel
+\Python\Python34\python.exe setup.py build bdist_egg
+\Python\Python27\python.exe setup.py build bdist_egg
+\Python\Python26\python.exe setup.py build bdist_egg

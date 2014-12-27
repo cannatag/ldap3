ca902d50af69b78f7721c0da03c22e3acb2e7b13
cannatag <cannatag@gmail.com>
2014-12-27 16:57:57 +0100

    default sphinx theme

--- pureLDAP/docs/manual/source/conf.py
+++ pureLDAP/docs/manual/source/conf.py
@@ -51,7 +51,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'pureLDAP'
-copyright = '2013 - ' + __author__
+copyright = '2015 - ' + __author__
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the

b00e49f6172239cb9730674493751c2e73aab02c
cannatag <cannatag@gmail.com>
2014-12-28 22:39:43 +0100

    added twine for upload to pyPI

--- build-dist.cmd
+++ build-dist.cmd
@@ -2,4 +2,4 @@ rd dist build pureLDAP\python3_ldap.egg-info /S /Q
 \Python\Python34\python.exe setup.py clean
 \Python\Python34\python.exe setup.py build sdist --format=gztar
 \Python\Python34\python.exe setup.py build bdist_wininst
-\Python\Python34\python.exe setup.py build bdist_wheel
+\Python\Python34\python.exe setup.py build bdist_wheel --universal

74a739dbaccbebba355c53e9da0289a96dec7735
cannatag <cannatag@gmail.com>
2015-10-28 22:29:42 +0100

    v0.9.9.2

--- ldap3/version.py
+++ ldap3/version.py
@@ -1,12 +1,12 @@
 # THIS FILE IS AUTO-GENERATED. PLEASE DO NOT MODIFY# version file for ldap3
-# generated on 2015-09-23 21:35:23.977877
+# generated on 2015-10-28 22:29:04.351053
 # on system uname_result(system='Windows', node='GCNBHPW8', release='post2012Server', version='6.3.9600', machine='AMD64', processor='Intel64 Family 6 Model 58 Stepping 9, GenuineIntel')
 # with Python 3.5.0 - ('v3.5.0:374f501f4567', 'Sep 13 2015 02:27:37') - MSC v.1900 64 bit (AMD64)
 #
-__version__ = '0.9.9.1'
+__version__ = '0.9.9.2'
 __author__ = 'Giovanni Cannata'
 __email__ = 'cannatag@gmail.com'
 __url__ = 'https://github.com/cannatag/ldap3'
-__description__ = 'A strictly RFC 4511 conforming LDAP V3 pure Python client. Same codebase for Python 2, Python3, PyPy and PyPy 3'
+__description__ = 'A strictly RFC 4510 conforming LDAP V3 pure Python client. Same codebase for Python 2, Python3, PyPy and PyPy 3'
 __status__ = 'development - beta'
 __license__ = 'LGPL v3'

36f734f630e04e063e2fd90aae8f99c88ad4777d
cannatag <cannatag@gmail.com>
2016-07-03 16:23:00 +0200

    v1.3.3 released

--- ldap3/version.py
+++ ldap3/version.py
@@ -1,9 +1,9 @@
 # THIS FILE IS AUTO-GENERATED. PLEASE DO NOT MODIFY# version file for ldap3
-# generated on 2016-07-02 09:46:41.546435
-# on system uname_result(system='Windows', node='GCNBHPW10', release='10', version='10.0.10586', machine='AMD64', processor='Intel64 Family 6 Model 58 Stepping 9, GenuineIntel')
-# with Python 3.5.2 - ('v3.5.2:4def2a2901a5', 'Jun 25 2016 22:18:55') - MSC v.1900 64 bit (AMD64)
+# generated on 2016-07-03 16:22:24.953000
+# on system ('Windows', 'GCNBHPW10', '10', '10.0.10586', 'AMD64', 'Intel64 Family 6 Model 58 Stepping 9, GenuineIntel')
+# with Python 2.7.12 - ('v2.7.12:d33e0cf91556', 'Jun 27 2016 15:24:40') - MSC v.1500 64 bit (AMD64)
 #
-__version__ = '1.3.2'
+__version__ = '1.3.3'
 __author__ = 'Giovanni Cannata'
 __email__ = 'cannatag@gmail.com'
 __url__ = 'https://github.com/cannatag/ldap3'

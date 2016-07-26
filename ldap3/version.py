00d813f072fa4f1ad60124aa35604b89a18d965a
cannatag <cannatag@gmail.com>
2016-07-26 21:57:34 +0200

    cert and key in same file

--- ldap3/version.py
+++ ldap3/version.py
@@ -1,5 +1,5 @@
 # THIS FILE IS AUTO-GENERATED. PLEASE DO NOT MODIFY# version file for ldap3
-# generated on 2016-07-19 00:36:26.167337
+# generated on 2016-07-19 00:38:26.476729
 # on system uname_result(system='Windows', node='GCNBHPW10', release='10', version='10.0.10586', machine='AMD64', processor='Intel64 Family 6 Model 58 Stepping 9, GenuineIntel')
 # with Python 3.5.2 - ('v3.5.2:4def2a2901a5', 'Jun 25 2016 22:18:55') - MSC v.1900 64 bit (AMD64)
 #

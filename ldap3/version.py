8f3a383d2239feca4431c8f3051853c4718fb21d
Giovanni Cannata <cannatag@gmail.com>
2016-09-14 15:13:36 +0200

    fixed setting default value in cursor

--- ldap3/version.py
+++ ldap3/version.py
@@ -1,9 +1,9 @@
 # THIS FILE IS AUTO-GENERATED. PLEASE DO NOT MODIFY# version file for ldap3
-# generated on 2016-09-10 04:52:49.577177
-# on system uname_result(system='Windows', node='GCNBHPW10', release='10', version='10.0.14393', machine='AMD64', processor='Intel64 Family 6 Model 58 Stepping 9, GenuineIntel')
+# generated on 2016-09-13 10:13:34.097247
+# on system uname_result(system='Windows', node='W10GC9227', release='10', version='10.0.14393', machine='AMD64', processor='Intel64 Family 6 Model 45 Stepping 7, GenuineIntel')
 # with Python 3.5.2 - ('v3.5.2:4def2a2901a5', 'Jun 25 2016 22:18:55') - MSC v.1900 64 bit (AMD64)
 #
-__version__ = '2.0.0'
+__version__ = '2.0.1'
 __author__ = 'Giovanni Cannata'
 __email__ = 'cannatag@gmail.com'
 __url__ = 'https://github.com/cannatag/ldap3'

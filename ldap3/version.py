215df336c647512010cb2dcce574ddc20668a5d9
cannatag <cannatag@gmail.com>
2017-05-12 06:44:52 +0200

    updated changelog

--- ldap3/version.py
+++ ldap3/version.py
@@ -1,5 +1,5 @@
 # THIS FILE IS AUTO-GENERATED. PLEASE DO NOT MODIFY# version file for ldap3
-# generated on 2017-05-07 15:45:00.689562
+# generated on 2017-05-07 19:43:56.199925
 # on system uname_result(system='Windows', node='ELITE10GC', release='10', version='10.0.14393', machine='AMD64', processor='Intel64 Family 6 Model 58 Stepping 9, GenuineIntel')
 # with Python 3.6.1 - ('v3.6.1:69c0db5', 'Mar 21 2017 18:41:36') - MSC v.1900 64 bit (AMD64)
 #

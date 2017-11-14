e298bfbc8084653a8cdc34f29558d307abd6616c
cannatag <cannatag@gmail.com>
2017-11-14 23:43:54 +0100

    version 2.4

--- ldap3/version.py
+++ ldap3/version.py
@@ -1,7 +1,7 @@
 # THIS FILE IS AUTO-GENERATED. PLEASE DO NOT MODIFY# version file for ldap3
-# generated on 2017-09-03 07:31:46.464924
-# on system uname_result(system='Windows', node='ELITE10GC', release='10', version='10.0.15063', machine='AMD64', processor='Intel64 Family 6 Model 58 Stepping 9, GenuineIntel')
-# with Python 3.6.1 - ('v3.6.1:69c0db5', 'Mar 21 2017 18:41:36') - MSC v.1900 64 bit (AMD64)
+# generated on 2017-11-14 23:42:40.424134
+# on system uname_result(system='Windows', node='ELITE10GC', release='10', version='10.0.16299', machine='AMD64', processor='Intel64 Family 6 Model 58 Stepping 9, GenuineIntel')
+# with Python 3.6.3 - ('v3.6.3:2c5fed8', 'Oct  3 2017 18:11:49') - MSC v.1900 64 bit (AMD64)
 #
 __version__ = '2.4'
 __author__ = 'Giovanni Cannata'

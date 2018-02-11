c77c64b312df4037926b27b94e19ae7e4f3e8e51
cannatag <cannatag@gmail.com>
2018-02-12 00:24:32 +0100

    fixed ad timestamp evaluation for integers

--- ldap3/protocol/formatters/formatters.py
+++ ldap3/protocol/formatters/formatters.py
@@ -100,6 +100,9 @@ def format_ad_timestamp(raw_value):
     if raw_value == b'9223372036854775807':  # max value to be stored in a 64 bit signed int
         return datetime.max  # returns datetime.datetime(9999, 12, 31, 23, 59, 59, 999999)
     timestamp = int(raw_value)
+    if timestamp < 0:  # ad timestamp cannot be negative
+        return raw_value
+
     try:
         return datetime.fromtimestamp(timestamp / 10000000.0 - 11644473600, tz=OffsetTzInfo(0, 'UTC'))  # forces true division in python 2
     except (OSError, OverflowError, ValueError):  # on Windows backwards timestamps are not allowed
--- ldap3/protocol/formatters/validators.py
+++ ldap3/protocol/formatters/validators.py
@@ -27,7 +27,7 @@ from datetime import datetime
 from calendar import timegm
 from uuid import UUID
 
-from ... import SEQUENCE_TYPES, STRING_TYPES
+from ... import SEQUENCE_TYPES, STRING_TYPES, NUMERIC_TYPES
 from .formatters import format_time, format_ad_timestamp
 from ...utils.conv import to_raw, to_unicode
 
@@ -192,7 +192,12 @@ def validate_ad_timestamp(input_value):
     valid_values = []
     changed = False
     for element in input_value:
-        if isinstance(element, STRING_TYPES):  # tries to check if it is already be a AD timestamp
+        if isinstance(element, NUMERIC_TYPES):
+            if 0 <= element <= 9223372036854775807:  # min and max for the AD timestamp starting from 12:00 AM January 1, 1601
+                valid_values.append(element)
+            else:
+                return False
+        elif isinstance(element, STRING_TYPES):  # tries to check if it is already be a AD timestamp
             if isinstance(format_ad_timestamp(to_raw(element)), datetime):  # valid Generalized Time string
                 valid_values.append(element)
             else:

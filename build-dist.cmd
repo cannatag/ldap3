bd39e9f91b8c5d574ddb89263c5750cf1459c96e
cannatag <cannatag@gmail.com>
2019-03-24 09:32:45 +0100

    v2.6

--- build-dist.cmd
+++ build-dist.cmd
@@ -1,7 +1,7 @@
 rd dist build ldap3.egg-info /S /Q
 py -3 setup.py clean
 py -3 setup.py build sdist --format=gztar
-py -3.5 setup.py build bdist_wininst
+py -3 setup.py build bdist_wininst
 py -3 setup.py build bdist_wheel --universal
 py -2.6 setup.py bdist_egg
 py -2.7 setup.py bdist_egg
--- ldap3/version.py
+++ ldap3/version.py
@@ -1,5 +1,5 @@
 # THIS FILE IS AUTO-GENERATED. PLEASE DO NOT MODIFY# version file for ldap3
-# generated on 2019-03-24 09:13:51.884877
+# generated on 2019-03-24 09:25:59.057943
 # on system uname_result(system='Windows', node='ELITE10GC', release='10', version='10.0.17763', machine='AMD64', processor='Intel64 Family 6 Model 58 Stepping 9, GenuineIntel')
 # with Python 3.7.2 - ('tags/v3.7.2:9a3ffc0492', 'Dec 23 2018 23:09:28') - MSC v.1916 64 bit (AMD64)
 #

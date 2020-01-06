2466e18ef6174570506eca62d188f54e48f2d0ad
cannatag <cannatag@gmail.com>
2020-01-06 14:18:05 +0100

    fixes from github

--- ldap3/utils/conv.py
+++ ldap3/utils/conv.py
@@ -72,7 +72,7 @@ def to_unicode(obj, encoding=None, from_server=False):
     if isinstance(obj, STRING_TYPES):  # python3 strings, python 2 unicode
         return obj
 
-    raise UnicodeError("Unable to convert type %s to unicode: %r" % (type(obj).__class__.__name__, obj))
+    raise UnicodeError("Unable to convert type %s to unicode: %r" % (obj.__class__.__name__, obj))
 
 
 def to_raw(obj, encoding='utf-8'):

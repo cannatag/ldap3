cannatag/ldap3

commit 191667cff76e874164dce94dafebf0acf344bd41

checking more start_tls operations

ldap3/strategy/restartable.py
@@ -28,7 +28,7 @@ import socket
 
 from .. import get_config_parameter
 from .sync import SyncStrategy
-from ..core.exceptions import LDAPSocketOpenError, LDAPOperationResult, LDAPMaximumRetriesError
+from ..core.exceptions import LDAPSocketOpenError, LDAPOperationResult, LDAPMaximumRetriesError, LDAPStartTLSError
 from ..utils.log import log, log_enabled, ERROR, BASIC
 
 
@@ -149,7 +149,12 @@ class RestartableStrategy(SyncStrategy):
                 try:  # reopening connection
                     self.connection.open(reset_usage=False, read_server_info=False)
                     if self._restart_tls:  # restart tls if start_tls was previously used
-                        self.connection.start_tls(read_server_info=False)
+                        if self.connection.start_tls(read_server_info=False):
+                            error = 'restart tls in restartable not successful' + (' - ' + self.connection.last_error if self.connection.last_error else '')
+                            if log_enabled(ERROR):
+                                log(ERROR, '%s for <%s>', error, self)
+                            self.unbind()
+                            raise LDAPStartTLSError(error)
                     if message_type != 'bindRequest':
                         self.connection.bind(read_server_info=False, controls=self._last_bind_controls)  # binds with previously used controls unless the request is already a bindRequest
                     if not self.connection.server.schema and not self.connection.server.info:

ldap3/core/connection.py
@@ -66,8 +66,7 @@ from .usage import ConnectionUsage
 from .tls import Tls
 from .exceptions import LDAPUnknownStrategyError, LDAPBindError, LDAPUnknownAuthenticationMethodError, \
     LDAPSASLMechanismNotSupportedError, LDAPObjectClassError, LDAPConnectionIsReadOnlyError, LDAPChangeError, LDAPExceptionError, \
-    LDAPObjectError, LDAPSocketReceiveError, LDAPAttributeError, LDAPInvalidValueError, LDAPConfigurationError, \
-    LDAPInvalidPortError
+    LDAPObjectError, LDAPSocketReceiveError, LDAPAttributeError, LDAPInvalidValueError, LDAPInvalidPortError, LDAPStartTLSError
 
 from ..utils.conv import escape_bytes, prepare_for_stream, check_json_dict, format_json, to_unicode
 from ..utils.log import log, log_enabled, ERROR, BASIC, PROTOCOL, EXTENDED, get_library_log_hide_sensitive_data
@@ -382,11 +381,22 @@ class Connection(object):
             if self.auto_bind == AUTO_BIND_NO_TLS:
                 self.bind(read_server_info=True)
             elif self.auto_bind == AUTO_BIND_TLS_BEFORE_BIND:
-                self.start_tls(read_server_info=False)
-                self.bind(read_server_info=True)
+                if self.start_tls(read_server_info=False):
+                    self.bind(read_server_info=True)
+                else:
+                    error = 'automatic start_tls befored bind not successful' + (' - ' + self.last_error if self.last_error else '')
+                    if log_enabled(ERROR):
+                        log(ERROR, '%s for <%s>', error, self)
+                    self.unbind()  # unbind anyway to close connection
+                    raise LDAPStartTLSError(error)
             elif self.auto_bind == AUTO_BIND_TLS_AFTER_BIND:
                 self.bind(read_server_info=False)
-                self.start_tls(read_server_info=True)
+                if not self.start_tls(read_server_info=True):
+                    error = 'automatic start_tls after bind not successful' + (' - ' + self.last_error if self.last_error else '')
+                    if log_enabled(ERROR):
+                        log(ERROR, '%s for <%s>', error, self)
+                    self.unbind()
+                    raise LDAPStartTLSError(error)
             if not self.bound:
                 error = 'automatic bind not successful' + (' - ' + self.last_error if self.last_error else '')
                 if log_enabled(ERROR):
@@ -1497,7 +1507,12 @@ class Connection(object):
                     if self._deferred_open:
                         self.open(read_server_info=False)
                     if self._deferred_start_tls:
-                        self.start_tls(read_server_info=False)
+                        if not self.start_tls(read_server_info=False):
+                            error = 'deferred start_tls not successful' + (' - ' + self.last_error if self.last_error else '')
+                            if log_enabled(ERROR):
+                                log(ERROR, '%s for <%s>', error, self)
+                            self.unbind()
+                            raise LDAPStartTLSError(error)
                     if self._deferred_bind:
                         self.bind(read_server_info=False, controls=self._bind_controls)
                     if (read_info is None and (not self.server.info and self.server.get_info in [DSA, ALL]) or (not self.server.schema and self.server.get_info in [SCHEMA, ALL])) or read_info:

ldap3/core/exceptions.py
@@ -598,12 +598,8 @@ def communication_exception_factory(exc_to_raise, exc):
         raise LDAPExceptionError('unable to generate exception type ' + str(exc_to_raise))
 
 
-def start_tls_exception_factory(exc_to_raise, exc):
+def start_tls_exception_factory(exc):
     """
     Generates a new exception class of the requested type merged with the exception raised by the interpreter
     """
-
-    if exc_to_raise.__name__ == 'LDAPStartTLSError':
-        return type(exc_to_raise.__name__, (exc_to_raise, type(exc)), dict())
-    else:
-        raise LDAPExceptionError('unable to generate exception type ' + str(exc_to_raise))
+    return type(LDAPStartTLSError.__name__, (LDAPStartTLSError, type(exc)), dict())

ldap3/core/tls.py
@@ -23,7 +23,7 @@
 # along with ldap3 in the COPYING and COPYING.LESSER files.
 # If not, see <http://www.gnu.org/licenses/>.
 
-from .exceptions import LDAPSSLNotSupportedError, LDAPSSLConfigurationError, LDAPStartTLSError, LDAPCertificateError, start_tls_exception_factory
+from .exceptions import LDAPSSLNotSupportedError, LDAPSSLConfigurationError, LDAPCertificateError, start_tls_exception_factory, LDAPStartTLSError
 from .. import SEQUENCE_TYPES
 from ..utils.log import log, log_enabled, ERROR, BASIC, NETWORK
 
@@ -286,7 +286,7 @@ class Tls(object):
             connection.last_error = 'wrap socket error: ' + str(e)
             if log_enabled(ERROR):
                 log(ERROR, 'error <%s> wrapping socket for TLS in <%s>', connection.last_error, connection)
-            raise start_tls_exception_factory(LDAPStartTLSError, e)(connection.last_error)
+            raise start_tls_exception_factory(e)(connection.last_error)
         finally:
             connection.starting_tls = False
 

ldap3/protocol/sasl/digestMd5.py
@@ -110,7 +110,6 @@ def sasl_digest_md5(connection, controls):
 
     a0 = md5_h(b':'.join([user, realm, password]))
     a1 = b':'.join([a0, nonce, cnonce, authz_id]) if authz_id else b':'.join([a0, nonce, cnonce])
-    print(connection.server.host.encode(charset))
     a2 = b'AUTHENTICATE:' + uri + (b':00000000000000000000000000000000' if qop in [b'auth-int', b'auth-conf'] else b'')
 
     digest_response += b'response="' + md5_hex(md5_kd(md5_hex(md5_h(a1)), b':'.join([nonce, b'00000001', cnonce, qop, md5_hex(md5_h(a2))]))) + b'"'

ldap3/strategy/base.py
@@ -37,7 +37,7 @@ from random import choice
 from .. import SYNC, ANONYMOUS, get_config_parameter, BASE, ALL_ATTRIBUTES, ALL_OPERATIONAL_ATTRIBUTES, NO_ATTRIBUTES
 from ..core.results import DO_NOT_RAISE_EXCEPTIONS, RESULT_REFERRAL
 from ..core.exceptions import LDAPOperationResult, LDAPSASLBindInProgressError, LDAPSocketOpenError, LDAPSessionTerminatedByServerError,\
-    LDAPUnknownResponseError, LDAPUnknownRequestError, LDAPReferralError, communication_exception_factory, \
+    LDAPUnknownResponseError, LDAPUnknownRequestError, LDAPReferralError, communication_exception_factory, LDAPStartTLSError, \
     LDAPSocketSendError, LDAPExceptionError, LDAPControlError, LDAPResponseTimeoutError, LDAPTransactionError
 from ..utils.uri import parse_uri
 from ..protocol.rfc4511 import LDAPMessage, ProtocolOp, MessageID, SearchResultEntry
@@ -788,7 +788,12 @@ class BaseStrategy(object):
                 referral_connection.open()
                 referral_connection.strategy._referrals = self._referrals
                 if self.connection.tls_started and not referral_server.ssl:  # if the original server was in start_tls mode and the referral server is not in ssl then start_tls on the referral connection
-                    referral_connection.start_tls()
+                    if not referral_connection.start_tls():
+                        error = 'start_tls in referral not successful' + (' - ' + referral_connection.last_error if referral_connection.last_error else '')
+                        if log_enabled(ERROR):
+                            log(ERROR, '%s for <%s>', error, self)
+                        self.unbind()
+                        raise LDAPStartTLSError(error)
 
                 if self.connection.bound:
                     referral_connection.bind()

test/config.py
@@ -42,7 +42,7 @@ from ldap3.utils.log import OFF, ERROR, BASIC, PROTOCOL, NETWORK, EXTENDED, set_
 from ldap3 import __version__ as ldap3_version
 from pyasn1 import __version__ as pyasn1_version
 
-test_strategy = getenv('STRATEGY', SAFE_SYNC)  # possible choices: SYNC, SAFE_SYNC, ASYNC, RESTARTABLE, REUSABLE, MOCK_SYNC, MOCK_ASYNC (not used on TRAVIS - look at .travis.yml)
+test_strategy = getenv('STRATEGY', ASYNC)  # possible choices: SYNC, SAFE_SYNC, ASYNC, RESTARTABLE, REUSABLE, MOCK_SYNC, MOCK_ASYNC (not used on TRAVIS - look at .travis.yml)
 test_server_type = getenv('SERVER', 'EDIR')  # possible choices: EDIR (Novell eDirectory), AD (Microsoft Active Directory), SLAPD (OpenLDAP)
 
 test_verbose = True if getenv('VERBOSE', 'FALSE').upper() == 'TRUE' else False

commit befa1f44028d2176965a4a9ab6c4347b86e3d609

fixed attribute error in restartable class (Fix #858)
This bug was introduced in commit 191667cf.

ldap3/strategy/restartable.py
@@ -153,7 +153,7 @@ class RestartableStrategy(SyncStrategy):
                             error = 'restart tls in restartable not successful' + (' - ' + self.connection.last_error if self.connection.last_error else '')
                             if log_enabled(ERROR):
                                 log(ERROR, '%s for <%s>', error, self)
-                            self.unbind()
+                            self.connection.unbind()
                             raise LDAPStartTLSError(error)
                     if message_type != 'bindRequest':
                         self.connection.bind(read_server_info=False, controls=self._last_bind_controls)  # binds with previously used controls unless the request is already a bindRequest
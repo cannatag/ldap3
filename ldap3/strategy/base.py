4c70935e595ec6a311c4560a6751da4376580b54
felixcapuano <felix.capuano@gmail.com>
2022-09-12 09:49:22 +0200

    catch KeyError when attributes doesn't exist

--- ldap3/strategy/base.py
+++ ldap3/strategy/base.py
@@ -435,10 +435,14 @@ class BaseStrategy(object):
                                     orig_attr, _, _ = attribute_type.partition(';')
                                     attrs_to_remove.append(orig_attr)
                             for attribute_type in attrs_to_remove:
-                                if log_enabled(PROTOCOL):
-                                    log(PROTOCOL, 'attribute type <%s> removed in response because of same attribute returned as range by the server in <%s>', attribute_type, self)
-                                del entry['raw_attributes'][attribute_type]
-                                del entry['attributes'][attribute_type]
+                                try:
+                                    del entry['raw_attributes'][attribute_type]
+                                    del entry['attributes'][attribute_type]
+
+                                    if log_enabled(PROTOCOL):
+                                        log(PROTOCOL, 'attribute type <%s> removed in response because of same attribute returned as range by the server in <%s>', attribute_type, self)
+                                except:
+                                    ...
 
             request = self._outstanding.pop(message_id)
         else:
